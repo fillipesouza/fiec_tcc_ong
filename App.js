62e5290443aae7f2ff0c82ffccc008caa5462c03
fillipesouza <ss.fillipe@gmail.com>
2020-06-13 17:16:18 -0300

    Corrigindo algumas partes

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import LoginScreen from "./src/screens/LoginScreen";
+import { combineReducers, createStore } from 'redux';
+import userReducer from './src/store/UserReducer';
+import { Provider } from 'react-redux';
+
+const rootReducer = combineReducers({
+  user: userReducer
+})
+
+const store = createStore(rootReducer);
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -15,12 +24,14 @@ const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
+    <Provider store={store} >
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </Provider>
   );
 };
 
--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function BlogScreen() {
   return (
     <View>
-      <Text></Text>
+      <Text>Hello</Text>
     </View>
   );
 }
